4f84f87eef3a5847e3819d9f216ca26cd026bb8f
handsone-u <jk04172@naver.com>
2021-10-22 15:25:41 +0900

    Bin-Search

--- src/bin_search/Baek2805.java
+++ src/bin_search/Baek2805.java
@@ -16,18 +16,34 @@ public class Baek2805 {
         n = Long.parseLong(nm[0]);
         m = Long.parseLong(nm[1]);
         arr = new long[(int) n];
+        long min = 0, max = 0;
 
-        for (int i = 0; i < n; i++) arr[i] = Long.parseLong(trees[i]);
-        Arrays.sort(arr);
-
-        long min = 0, max = arr[(int) (n - 1)];
-        while (min <= max) {
-            long
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(trees[i]);
+            if(max<arr[i]) max = arr[i];
         }
 
+        while (min <= max) {
+            long middle = (min + max) / 2;
+            long tmpM = getLog(middle);
+            if (tmpM >= m) {
+                min = middle + 1;
+                if(h<middle) h = middle;
+            }
+            else max = middle - 1;
+        }
 
         writer.write(Long.toString(h));
         writer.flush();
         writer.close();
+    }
+
+    public static long getLog(long height) {
+        long result = 0;
+        for (int i = 0; i < n; i++) {
+            if(arr[i]<=height) continue;
+            result += arr[i] - height;
+        }
+        return result;
     }
 }
